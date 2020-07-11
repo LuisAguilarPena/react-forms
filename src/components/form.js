700d48cbf2f10410d98cb66bac5fe08d4da59611
Luis Aguilar <luis.aguilar@enroutesystems.com>
2020-07-11 14:15:19 -0500

    getting the names from the restful service into the menu items of the country select field

--- src/components/form.js
+++ src/components/form.js
@@ -1,44 +1,62 @@
 import React from "react";
 import { TextField, Radio, MenuItem, FormControl, FormLabel, RadioGroup, FormControlLabel } from '@material-ui/core';
 
-export default function Form() {
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      countries: [
+        {name: "Test"}
+      ]
+    }
+  }
 
-  fetch('https://restcountries.eu/rest/v2/all')
-    .then(response => response.json())
-    .then(data => {
-        console.log("-->", data);
-        
-    })
-    .catch(err => {
-        console.error('An error ocurred', err);
-    });
-
-
-  return (
-    <form noValidate autoComplete="off" style={{display: 'flex', flexDirection: 'column'}}>
-      <TextField label="Name" />
-      <TextField label="Last Name" />
-      <TextField label="E-mail" />
-      
-      <TextField label="Country" 
-        value=""
-        select 
-      >
-        <MenuItem value="USA">USA</MenuItem>
-      </TextField> <br/>
-
-      <FormControl component="fieldset">
-        <FormLabel component="legend">Gender</FormLabel>
-        <RadioGroup aria-label="gender" name="gender1" value="" >
-          <FormControlLabel value="female" control={<Radio />} label="Female" />
-          <FormControlLabel value="male" control={<Radio />} label="Male" />
-          <FormControlLabel value="other" control={<Radio />} label="Other" />
-        </RadioGroup>
-      </FormControl>
+  componentDidMount() {
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({countries: data});
+      })
+      .catch(err => {
+          console.error('An error ocurred', err);
+      });
+  }
 
 
-      <TextField label="Teléfono" />
-
-    </form>
-  )
+  
+  render() {
+    const { countries } = this.state;
+    console.log("DATA -->", countries);
+      return (
+        <form noValidate autoComplete="off" style={{display: 'flex', flexDirection: 'column'}}>
+          <TextField label="Name" />
+          <TextField label="Last Name" />
+          <TextField label="E-mail" />
+          
+          <TextField label="Country" 
+            value=""
+            select 
+          >
+            {countries.map( country => 
+              <MenuItem key={country.name} value={country.name} >
+                {country.name}
+              </MenuItem>
+            )}
+          </TextField> <br/>
+    
+          <FormControl component="fieldset">
+            <FormLabel component="legend">Gender</FormLabel>
+            <RadioGroup aria-label="gender" name="gender1" value="" >
+              <FormControlLabel value="female" control={<Radio />} label="Female" />
+              <FormControlLabel value="male" control={<Radio />} label="Male" />
+              <FormControlLabel value="other" control={<Radio />} label="Other" />
+            </RadioGroup>
+          </FormControl>
+    
+    
+          <TextField label="Teléfono" />
+    
+        </form>
+      )
+    }
 }
